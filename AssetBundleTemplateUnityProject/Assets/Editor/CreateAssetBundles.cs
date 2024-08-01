435745a291fbbd8fff9a55fb720647af21dcb38b
chaz3692 <chazzybcool@outlook.com>
2024-08-01 16:06:17 +0800

    new stump layout

---
+++ AssetBundleTemplateUnityProject/Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundleOutput";
+        if (!Directory.Exists(Application.streamingAssetsPath))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+    }
+}
